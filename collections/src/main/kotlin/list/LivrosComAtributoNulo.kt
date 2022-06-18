a972875a5a704468cdc348da6f677905b8eec2be
João Vitor Costa <dev.es.costa@gmail.com>
2022-06-18 19:17:40 -0300

    coleção de dados

---
+++ collections/src/main/kotlin/list/LivrosComAtributoNulo.kt
@@ -0,0 +1,9 @@
+package list
+
+fun main() {
+    listaDeLivros
+        .groupBy { it.editora ?: "Editora desconhecida" }
+        .forEach { (editora: String, livros: List<Livro>) ->
+            println("$editora: ${livros.joinToString { it.titulo }}")
+        }
+}
