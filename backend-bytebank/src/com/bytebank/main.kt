dff72cfa9ad932934ea380d00bcc679aae4d429d
João Vitor Costa <dev.es.costa@gmail.com>
2022-06-18 13:51:08 -0300

    Reorganizando projeto

---
+++ backend-bytebank/src/com/bytebank/main.kt
@@ -0,0 +1,32 @@
+package com.bytebank
+
+import com.bytebank.model.Endereco
+
+fun main() {
+    val endereco = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    )
+    val enderecoNovo = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-0700"
+    )
+
+    println(endereco.equals(enderecoNovo))
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+    println(endereco)
+    println(enderecoNovo)
+
+    println("${endereco.javaClass}@${
+        Integer.toHexString(endereco.hashCode())}")
+}
+
+fun imprime(valor: Any): Any {
+    println(valor)
+    return valor
+}
